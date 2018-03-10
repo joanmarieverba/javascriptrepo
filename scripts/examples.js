5f9d204ceafe9e0589c33cc107b415ba75f3ff7b
Joan Marie Verba <verba001@umn.edu>
2018-03-10 14:02:31 -0600

    classwork

--- scripts/examples.js
+++ scripts/examples.js
@@ -1,2 +1,58 @@
 'use strict';
 
+// for (var i=0; i < 5, i++){
+//     console.log(i);
+// }
+
+// for (let i = 0; i < 5, i++) {
+//     console.log(i);
+// }
+
+// const testArray = ['a', 'b', "c", "d"];
+
+// const testArray = [1, 2, 3, 4];
+
+
+
+for (let i = 0; i < testArray.length; i++) {
+  console.log(testArray[i]);
+}
+
+let j = 0;
+while (j < testArray.length) {
+  console.log(testArray[j]);
+  j++;
+}
+// same as the for loop above
+testArray.forEach(function (element) {
+  const result = element + 1;
+  console.log(element);
+});
+
+const newArray = [];
+testArray.forEach(function (element) {
+  const result = element + 1;
+  newArray.push(result);
+  console.log(element);
+});
+console.log(newArray);
+
+// iterates over the array element with each item and returns a new array
+// cannot map an array onto itself
+const newerArray = testArray.map(function (item) {
+  return item + 1;
+});
+
+console.log(newerArray);
+
+const testArray = ["bob", "sue", "harry"];
+
+testArray.forEach(function (element) {
+    console.log(`${element} is super kewl');
+});
+
+
+
+setTimeout(function (){
+        alert ("It's been 5 seconds");
+    }, 1000);
---
+++ scripts/functional.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const testArray = [1, 2, 3, 4, 5, 6];
+
+
+// "return" keyword is essential
+const mappedArray = testArray.map(function (elem) {
+  return elem + 1;
+});
+
+console.log(mappedArray);
+
+// "filter" only returns element that meets a test condition
+
+const filteredArray = mappedArray.filter(function(elem) {
+  return elem % 2 === 0; // return only even numbers
+});
+
+// reduce accumulates the elements of an array and gives one result
+const reducedArray = filteredArray.reduce(function(acc, elem) {
+  return acc + elem;
+}, 0); // tells reduce function what you want accumulator to be...here it's one value
+// zero is the starting number, says the result is a number, too
